ccc7578c327ecb3cae6ab8018d6f1bf16a0786cc
Andrew Trent <andrewtrentdesign@gmail.com>
2018-02-02 05:27:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,16 +1,26 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  l_cell = "   |"
-  r_cell = "   \n"
+  def br
+    print("\n")
+  end
+  def l_cell 
+    2.times do
+      print("    |")
+    end
+  end
+  def r_cell 
+    print("   \n")
+  end
   def sep 
     11.times do 
       print "-" 
     end
   end
   def row
-    puts(l_cell)
-    print(l_cell)
-    print(r_cell)
+    br
+    l_cell
+    l_cell
+    r_cell
   end
   row
   sep
